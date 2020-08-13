e200f76efbf31f465764497288760329982dd2b2
DavisDeco <you@example.com>
2020-08-13 16:02:54 +0300

    finished everything

--- varalhub/varalhub/urls.py
+++ varalhub/varalhub/urls.py
@@ -6,5 +6,10 @@ from .views import *
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('account', createAccount, name='my-account'),
+    path('wishlist', wishlist, name='wishlist'),
+    path('mycart', mycart, name='mycart'),
+    path('checkout', checkout, name='checkout'),
+    path('contact', contactus, name='contact'),
 
 ]
--- varalhub/varalhub/views.py
+++ varalhub/varalhub/views.py
@@ -7,32 +7,19 @@ import json
  
 
 def home(request):
-
-    # if request.is_ajax() and request.method == "GET":
-    #     residentialData = {}
-    #     officesData = {}
-    #     # agentsData = {}
-
-    #     try:
-    #         residentials = requests.get(settings.RESIDENTIAL_FEATURED_API_URL,timeout=10)
-    #         offices = requests.get(settings.OFFICE_FEATURED_API_URL,timeout=10)
-    #         # agents = requests.get(settings.FEATURED_AGENT_API_URL,timeout=10)
-
-    #         if residentials.status_code == 200:
-    #             if json.loads(residentials.content)['results']:
-    #                 residentialData = json.loads(residentials.content)['results']
-
-    #         if offices.status_code == 200:
-    #             if json.loads(offices.content)['results']: 
-    #                 officesData = json.loads(offices.content)['results'] 
-            
-    #         # if agents.status_code == 200:
-    #         #     if json.loads(agents.content)['results']: 
-    #         #         agentsData= json.loads(agents.content)['results'] 
-            
-    #         return HttpResponse(json.dumps({"success":"Retrived all featured data","residentialData":residentialData,"officesData":officesData}),status=200,content_type="application/json")
-
-    #     except requests.exceptions.RequestException as e:
-    #         return HttpResponse(json.dumps({"error":"Featured data not retrieved"}),status=404,content_type="application/json") 
-
   return render(request,"index.html",{})
+
+def createAccount(request):
+  return render(request,"account.html",{})
+
+def wishlist(request):
+  return render(request,"wishlist.html",{})
+
+def mycart(request):
+  return render(request,"cart.html",{})
+
+def checkout(request):
+  return render(request,"checkout.html",{})
+
+def contactus(request):
+  return render(request,"contactus.html",{})
